e2202a463fd61f3dd78ff3e96c3532e443aa9c6c
Doan Bui <bxdoan93@gmail.com>
2024-10-15 14:32:04 +0700

    test schedule

--- src/main/java/example/restservice/RestServiceApplication.java
+++ src/main/java/example/restservice/RestServiceApplication.java
@@ -2,11 +2,12 @@ package example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
 //
 // https://github.com/spring-guides/gs-rest-service
 
 @SpringBootApplication
+@EnableScheduling
 public class RestServiceApplication {
 
     public static void main(String[] args) {
